0604326bd6521cebea58e09008c0541c2ae2d776
Alexander Rondon <arondn2@gmail.com>
2020-10-05 16:46:00 -0500

    test: test change invoke, send and reject methods for events

--- src/__tests__/unit/websocket.sequence.test.ts
+++ src/__tests__/unit/websocket.sequence.test.ts
@@ -4,6 +4,7 @@ import { getNewFactory } from '../fixtures/application';
 import { Socket } from 'socket.io';
 import { DummySocket } from '../fixtures/dummy-socket';
 import { WebsocketControllerFactory } from '../../websocket-controller-factory';
+import { WebsocketBindings } from '../../keys';
 
 export class SequenceTestController {
   responseSuccess({ oneParam }: { oneParam: string }) {
@@ -27,32 +28,97 @@ describe('WebsocketSequence', () => {
     });
   };
 
-  before(async () => {
-    app = new WebsocketApplication();
-    app.websocketServer.controller(SequenceTestController);
-    factory = getNewFactory(app, SequenceTestController);
-    await factory.createController((dummySocket as Object) as Socket);
+  describe('with a default providers values', () => {
+    before(async () => {
+      app = new WebsocketApplication();
+      app.websocketServer.controller(SequenceTestController);
+      factory = getNewFactory(app, SequenceTestController);
+      await factory.createController((dummySocket as Object) as Socket);
+    });
+
+    it('callback response with success', async () => {
+      const text = 'this is first param';
+      const response = await callMethod('responseSuccess', {
+        oneParam: text,
+      });
+      expect(!!response).to.be.true();
+      expect(response).to.be.eql({
+        result: { text: `yes you are the first params: ${text}` },
+      });
+    });
+
+    it('callback response with error handling', async () => {
+      const text = 'this is another param';
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      const response: any = await callMethod('responseError', {
+        badParam: text,
+      });
+      expect(!!response).to.be.true();
+      expect(!!response.error).to.be.true();
+      expect(response.error).to.have.containEql({
+        message: `this is a badParam: ${text}`,
+      });
+    });
   });
 
-  it('callback response with success', async () => {
-    const text = 'this is first param';
-    const response = await callMethod('responseSuccess', {
-      oneParam: text,
-    });
-    expect(!!response).to.be.true();
-    expect(response).to.be.eql({
-      result: { text: `yes you are the first params: ${text}` },
-    });
-  });
+  describe('with a distinct providers values', () => {
+    before(async () => {
+      app = new WebsocketApplication();
+      app.websocketServer.controller(SequenceTestController);
+      factory = getNewFactory(app, SequenceTestController);
+      await factory.createController((dummySocket as Object) as Socket);
 
-  it('callback response with error handling', async () => {
-    const text = 'this is another param';
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const response: any = await callMethod('responseError', { badParam: text });
-    expect(!!response).to.be.true();
-    expect(!!response.error).to.be.true();
-    expect(response.error).to.have.containEql({
-      message: `this is a badParam: ${text}`,
+      app
+        .bind(WebsocketBindings.INVOKE_METHOD)
+        .to(async (methodName: string, args: unknown[]) => {
+          if (methodName === 'responseSuccess') {
+            return {
+              invoqueMethod: 'customInvoqueMethod',
+              methodName,
+              args,
+            };
+          }
+          throw new Error('Bad method name');
+        });
+
+      app
+        .bind(WebsocketBindings.SEND_METHOD)
+        .to(async (done: Function, result: unknown) => {
+          done({ myBody: result });
+        });
+
+      app
+        .bind(WebsocketBindings.REJECT_METHOD)
+        .to(async (done: Function, error: Error) => {
+          done({ myAppErrorMessage: error.message });
+        });
+    });
+
+    it('callback response with success', async () => {
+      const text = 'this is first param';
+      const response = await callMethod('responseSuccess', {
+        oneParam: text,
+      });
+      expect(!!response).to.be.true();
+      expect(response).to.be.eql({
+        myBody: {
+          invoqueMethod: 'customInvoqueMethod',
+          methodName: 'responseSuccess',
+          args: [{ oneParam: text }],
+        },
+      });
+    });
+
+    it('callback response with error handling', async () => {
+      const text = 'this is another param';
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      const response: any = await callMethod('responseError', {
+        badParam: text,
+      });
+      expect(!!response).to.be.true();
+      expect(response).to.have.containEql({
+        myAppErrorMessage: `Bad method name`,
+      });
     });
   });
 });
